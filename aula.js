e6af2039acf0ced8f3b27726b202237e31909a41
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-11-22 21:55:37 -0300

    Create aula.js

---
+++ aula.js
@@ -0,0 +1,12 @@
+// let idade = 5;
+// console.log(idade);
+
+let valorPassagem = 39;
+valorPassagem = 30;
+console.log(valorPassagem);
+
+// const valorPassagem = 39;
+// valorPassagem = 30;
+// console.log(valorPassagem);
+// // Isso gera um erro, pois o tipo de variavel é uma constante e não pode ser alterada como foi feito na linha 9
+
