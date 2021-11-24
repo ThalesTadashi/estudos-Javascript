30922a5f8b200e825b19647f39cd2c42050a0262
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-11-24 19:38:39 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -10,3 +10,20 @@ console.log(valorPassagem);
 // console.log(valorPassagem);
 // // Isso gera um erro, pois o tipo de variavel é uma constante e não pode ser alterada como foi feito na linha 9
 
+let nome = 'Thales'; //script literal
+let idade = 22; //number literal
+let aprovado = true; //boolean
+let sobrenome = undefined; //undefined
+let corSelecionada = null; //redefinir
+
+//typeof nome; // diz o tipo da variavel 
+
+//Objetos
+
+let pessoa = {
+nome : 'Thales',
+idade : 22,
+maiorIdade : true,
+corPredileta : null
+};
+console.log(pessoa );
